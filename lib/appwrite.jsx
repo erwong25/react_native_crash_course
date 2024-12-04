78599d3bff8b8513e799d97d013545b9092f117f
pukapunch <pukapunch@gmail.com>
2024-12-03 17:57:55 -0800

    first commit

---
+++ lib/appwrite.jsx
@@ -0,0 +1,89 @@
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  Query,
+} from "react-native-appwrite";
+import { ID } from "react-native-appwrite";
+
+export const config = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.jsm.aora",
+  projectId: "674f9ad0001ef990c62b",
+  databaseId: "674f9c75001b89912b6f",
+  userCollectionId: "674f9c90002734965c22",
+  videoCollectionId: "674f9ca6003e67c27a6e",
+  storageId: "674f9de2002f3b79d7e7",
+};
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+  .setEndpoint(config.endpoint)
+  .setProject(config.projectId)
+  .setPlatform(config.platform);
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+export async function createUser(email, password, username) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+    if (!newAccount) throw Error;
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      config.databaseId,
+      config.userCollectionId,
+      ID.unique({
+        accountID: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      })
+    );
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+}
+
+export async function signIn(email, password) {
+  try {
+    const session = await account.createEmailSession(email, password);
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+
+    if (!currentAccount) throw Error;
+
+    const currentUser = await database.listDocuments(
+      config.databaseId,
+      config.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+  }
+};
